7aa17d0fcde5d7e78ae49e072628dca470797681
truongtx91 <truongtx91@gmail.com>
2019-02-23 13:02:18 +0700

    setup MVVM model

---
+++ Tracking/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Unity;
+using Prism.Ioc;
+using Tracking.Views;
+using System.Windows;
+using Prism.Regions;
+using System.Windows.Controls;
+
+namespace Tracking
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : PrismApplication
+  {
+    protected override Window CreateShell()
+    {
+      return Container.Resolve<MainWindow>();
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+
+    }
+
+    // create a custom region adapter for the stackPannel
+    protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings)
+    {
+      base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+      regionAdapterMappings.RegisterMapping(typeof(StackPanel), Container.Resolve<StackPanelRegionAdapter>());
+    }
+  }
+}
---
+++ Tracking/Views/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Prism.Ioc;
+using Prism.Regions;
+using System.Windows;
+
+namespace Tracking.Views
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private IContainerExtension _container;
+    private IRegionManager _regionManager;
+
+    public MainWindow(IContainerExtension container, IRegionManager regionManager)
+    {
+      InitializeComponent();
+      _container = container;
+      _regionManager = regionManager;
+      // View discovery: automatically inject views with View discovery
+      //regionManager.RegisterViewWithRegion("ContentMap", typeof(MapView));
+      //regionManager.RegisterViewWithRegion("ContentChart", typeof(ChartView));
+      //regionManager.RegisterViewWithRegion("ContentCamera", typeof(CameraView));
+    }
+
+    private void Map_ItemClick(object sender, RoutedEventArgs e)
+    {
+      var view = _container.Resolve<MapView>();
+      IRegion region = _regionManager.Regions["ContentMap"];
+      region.Add(view);
+    }
+
+    private void Camera_ItemClick(object sender, RoutedEventArgs e)
+    {
+      var view = _container.Resolve<CameraView>();
+      IRegion region = _regionManager.Regions["ContentChart"];
+      region.Add(view);
+    }
+
+    private void Char_ItemClick(object sender, RoutedEventArgs e)
+    {
+      var view = _container.Resolve<ChartView>();
+      IRegion region = _regionManager.Regions["ContentChart"];
+      region.Add(view);
+    }
+  }
+}
